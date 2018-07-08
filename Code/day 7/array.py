2880a26a87248966bbae17a7e7eeb73d2c5ee152
ashis-h <32663261+ashis-h@users.noreply.github.com>
2018-07-08 22:29:23 +0530

    array

---
+++ Code/day 7/array.py
@@ -0,0 +1,5 @@
+print ("The new created array is : ",end="")
+for i in range (0,3):
+    print (arr[i], end=" ")
+ 
+print ("\r")
---
+++ Code/day 7/initialarray.py
@@ -0,0 +1,6 @@
+data1={'Id':100, 'Name':'Suresh', 'Profession':'Developer'}  
+data2={'Id':101, 'Name':'Ramesh', 'Profession':'Trainer'}  
+print "Id of 1st employer is",data1['Id']  
+print "Id of 2nd employer is",data2['Id']  
+print "Name of 1st employer:",data1['Name']  
+print "Profession of 2nd employer:",data2['Profession']  
