a7d495867923e4ca875fbadc1f97f98b5e0aef92
ashis-h <32663261+ashis-h@users.noreply.github.com>
2018-07-09 12:51:49 +0530

    2

---
+++ Code/day 8/MaxPairProduct.py
@@ -0,0 +1,23 @@
+def maxProduct(arr, n):
+ 
+    if (n < 2):
+        print("No pairs exists")
+        return
+     
+    
+    a = arr[0]; b = arr[1]
+ 
+    
+    for i in range(0, n):
+         
+        for j in range(i + 1, n):
+            if (arr[i] * arr[j] > a * b):
+                a = arr[i]; b = arr[j]
+ 
+    print("Max product pair is {", a, ",", b, "}",
+                                          sep = "")
+     
+
+arr = [1, 5, 6, -3, 10, -12]
+n = len(arr)
+maxProduct(arr, n)
