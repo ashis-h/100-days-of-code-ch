2f3605fe79680951687335bad225fae53d628b72
ashis-h <32663261+ashis-h@users.noreply.github.com>
2018-07-16 22:21:40 +0530

    3

---
+++ Code/Day 12/MinToMaxHeap.py
@@ -0,0 +1,13 @@
+def max_heapify(A, heap_size, i):
+    left = 2 * i + 1
+    right = 2 * i + 2
+    largest = i
+    if left < heap_size and A[left] > A[largest]:
+        largest = left
+    else: 
+        largest = i
+    if right < heap_size and A[right] > A[largest]:
+        largest = right
+    if largest != i:
+        A[i], A[largest] = A[largest], A[i]
+        max_heapify(A, heap_size, largest)
