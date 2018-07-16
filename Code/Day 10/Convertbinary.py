ad5f9d4e7b7b1c8a4e529c1a2ff22f8303696c53
ashis-h <32663261+ashis-h@users.noreply.github.com>
2018-07-16 22:21:09 +0530

    1

---
+++ Code/Day 10/Convertbinary.py
@@ -0,0 +1,11 @@
+def int_to_bin_string(i):
+    if i == 0:
+        return "0"
+    s = ''
+    while i:
+        if i & 1 == 1:
+            s = "1" + s
+        else:
+            s = "0" + s
+        i //= 2
+    return s
